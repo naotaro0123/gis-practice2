8920e1d8fd7a9e7e743f99e9dc68ad2266b32809
naotaro0123 <n.ikewada@gmail.com>
2025-01-13 14:19:13 +0900

    fix: add sky

--- src/maplibre-map-layer.ts
+++ src/maplibre-map-layer.ts
@@ -10,6 +10,7 @@ export const setupMapLayer = (container: HTMLElement) => {
   const map = new maplibregl.Map({
     container,
     center: [139.4894, 35.339],
+    maxPitch: 80,
     zoom: 16,
     style: {
       version: 8,
@@ -188,6 +189,32 @@ export const setupMapLayer = (container: HTMLElement) => {
         source: "aws-terrain",
         // 標高の誇張度
         exaggeration: 1,
+      },
+      sky: {
+        // 空のベースカラー
+        "sky-color": "#199EF3",
+        // 空の色と水平線の色の混ぜ合わせ。1は空の真ん中の色を、0は空の色を使用する
+        "sky-horizon-blend": 0.5,
+        // 地平線のベースカラー
+        "horizon-color": "#ffffff",
+        // 霧の色と水平線の色の混ぜ合わせ。0は水平線の色、1は霧の色を使用する
+        "horizon-fog-blend": 0.5,
+        // 霧のベースカラー。 3D地形が必要
+        "fog-color": "#0000ff",
+        // 3D地形に霧を混ぜ合わせる。 0はマップの中心、1は地平線
+        "fog-ground-blend": 0.5,
+        // 大気の混ぜ合わせ。 1が可視大気、0が非表示大気
+        "atmosphere-blend": [
+          "interpolate",
+          ["linear"],
+          ["zoom"],
+          0,
+          1,
+          10,
+          1,
+          12,
+          0,
+        ],
       },
     },
   });
